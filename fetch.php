bc04bd052e41793e56054aaa942f9f5e6f96bfdd
Aadil Prabhakar <aadilprabhakar@gmail.com>
2013-04-18 03:46:54 +0530

    jQuery CDN changed to GOOGLE Hosting Library

--- fetch.php
+++ fetch.php
@@ -11,8 +11,8 @@
 			background-color: #ccc;
 		}    
 	</style>
-  	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  	<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
 </head>
 <body>
 
